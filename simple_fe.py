7ece5eda88427e9a2ae784e6f75471ef26cf88ec
Apoorva Rao Balevalachilu <apoorva.rao.b@gmail.com>
2014-12-03 15:59:45 -0500

    corrected stripout

--- simple_fe.py
+++ simple_fe.py
@@ -1,6 +1,8 @@
 import string
 import re
 import nltk
+#import codecs
+#import sys
 
 def read_file(filename):
     r"""Assume the file is the format
@@ -49,8 +51,13 @@ def extract_features_for_sentence1(tokens):
         w_after = w_after.decode('utf-8')
 
         digitre = re.compile('.*[\d].*')
-
-        feats_per_position[t].add("word=%(word)s\tcap=%(isupper)i\tdigits=%(containsDigit)i\tstripOut=%(stripOut)s\tlowercased=%(lowercased)s\tshape=%(shape)s\tpostag=%(postag)s\tpostag_context=%(postag_context)s\tprev_word=%(prev_word)s\tnext_word=%(next_word)s"%{"word":w, "isupper":w[0].isupper(), "containsDigit":bool(digitre.search(w)), "stripOut":strip_feature(w), "lowercased":w.lower(), "shape":shape_feature(w), "postag":pos_tag(w), "postag_context":pos_tag_context(w_before,w,w_after),"prev_word":prev_word(w_before,w),"next_word":next_word(w,w_after)})
+        
+        
+        stripstring = ""
+        for featureType in ["hashtag", "mention", "retweet","url","symoticon", "apossuffix", "date"]:
+            stripstring += "\t" + featureType + "=" + str(strip_feature(featureType, w))
+        
+        feats_per_position[t].add("word=%(word)s\tcap=%(isupper)i\tdigits=%(containsDigit)i"+stripstring+"\tlowercased=%(lowercased)s\tshape=%(shape)s\tpostag=%(postag)s\tpostag_context=%(postag_context)s\tprev_word=%(prev_word)s\tnext_word=%(next_word)s"%{"word":w, "isupper":w[0].isupper(), "containsDigit":bool(digitre.search(w)), "lowercased":w.lower(), "shape":shape_feature(w), "postag":pos_tag(w), "postag_context":pos_tag_context(w_before,w,w_after),"prev_word":prev_word(w_before,w),"next_word":next_word(w,w_after)})
 
     return feats_per_position
 
@@ -58,22 +65,22 @@ extract_features_for_sentence = extract_features_for_sentence1
 
 #Anything that is definitely not an NE
 #character affixes and some wordform features
-def strip_feature(t): 
-    if re.match('#.*',t): #Hashtag
+def strip_feature(mode, t):
+    if mode == "hashtag" and re.match('#.*',t): #Hashtag
         return True
-    elif re.match('@.*',t): #mention
+    elif mode == "mention" and re.match('@.*',t): #mention
         return True
-    elif re.match('RT|Retweet',t): #Retweet
+    elif mode == "retweet" and re.match('RT|Retweet',t): #Retweet
         return True
-    elif re.match('((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)',t): #url
+    elif mode == "url" and re.match('((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)',t): #url
         return True
-    elif re.match("[<>.?!$@!%^;&*_\-=+():|,'\"\\/\[\]pPD3]+", t): #Emoticons and symbols
-        #[0-9A-Za-z'\&\-\.\/()=:;]+
+    elif mode == "symoticon" and re.match("[<>.?!$@!%^;&*_\-=+():|,'\"\\/\[\]pPD3]+", t): #Emoticons and symbols
         return True
-    elif re.match("'s", t): #Emoticons and symbols
+    elif mode == "apossuffix" and re.match("'s", t): #Emoticons and symbols
         return True
-    elif re.match("^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$",t): #dates
+    elif mode == "date" and re.match("^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$",t): #dates
         return True
+    
     return False
 
 #shape feature
@@ -135,16 +142,21 @@ def next_word(t, t_after):
 def extract_features_for_file(input_file, output_file):
     """This runs the feature extractor on input_file, and saves the output to
     output_file."""
+    #UTF8Writer = codecs.getwriter('utf8')
+    #sys.stdout = UTF8Writer(sys.stdout)
     sents = read_file(input_file)
     with open(output_file,'w') as output_fileobj:
         for tokens,goldtags in sents:
             feats = extract_features_for_sentence(tokens)
             for t in range(len(tokens)):
                 feats_tabsep = "\t".join(feats[t])
-                #feats_tabsep.encode('utf-8')
+                feats_tabsep = unicode(feats_tabsep)
+                #print feats_tabsep
+                feats_tabsep = feats_tabsep.encode('utf-8', errors='ignore')
                 print>>output_fileobj, "%s\t%s" % (goldtags[t], feats_tabsep)
             print>>output_fileobj, ""
 
 extract_features_for_file("train.txt", "train.feats")
 #extract_features_for_file("dev.txt", "dev.feats")
 extract_features_for_file("test_nolabels.txt", "test_nolabels.feats")
+
